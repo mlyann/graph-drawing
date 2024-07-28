1987f3805d4c8ad937502fbf0c4665347ab9301a
Ming <mlyang721@arizona.edu>
2024-07-28 18:46:29 -0400

    change wrong stress

--- graph_with_polygons.py
+++ graph_with_polygons.py
@@ -29,26 +29,6 @@ def create_polygon(x: float, y: float, radius: float, n_sides: int) -> Polygon:
 def generate_random_points(num_points: int) -> torch.Tensor:
     return torch.rand(size=(num_points, 2), requires_grad=True)
 
-def draw_graph_with_polygons(graph: GraphWithPolygons, points: torch.Tensor, n_sides: int, title: str):
-    fig, ax = plt.subplots()
-    pos = {node: (points[i][0].item(), points[i][1].item()) for i, node in enumerate(graph.graph.nodes())}
-
-    for node, (x, y) in pos.items():
-        radius = graph.radii[list(graph.graph.nodes()).index(node)].item()
-        polygon = create_polygon(x, y, radius, n_sides)
-        ax.add_patch(polygon)
-        ax.text(x, y, node, ha='center', va='center', fontsize=12)
-
-    for edge in graph.graph.edges():
-        x1, y1 = pos[edge[0]]
-        x2, y2 = pos[edge[1]]
-        ax.plot([x1, x2], [y1, y2], color='gray')
-
-    plt.title(title)
-    plt.axis('equal')
-    plt.axis('off')
-    plt.show()
-
 def node_overlap_loss(pos: torch.Tensor, radii: torch.Tensor, sample_size=None, sample=None) -> torch.Tensor:
     pairwise_distance = nn.PairwiseDistance()
     relu = nn.ReLU()
@@ -71,3 +51,32 @@ def node_overlap_loss(pos: torch.Tensor, radii: torch.Tensor, sample_size=None, 
     loss = relu(1 - normalized_dist).pow(2).mean()
 
     return loss
+
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+
+def create_polygon(x: float, y: float, radius: float, n_sides: int) -> Polygon:
+    theta = np.linspace(0, 2 * np.pi, n_sides, endpoint=False)
+    vertices = np.vstack((np.cos(theta), np.sin(theta))).T * radius + np.array([x, y])
+    return Polygon(vertices, closed=True, color='lightblue', ec='black')
+
+def draw_graph_with_polygons(graph: GraphWithPolygons, points: torch.Tensor, n_sides: int, title: str):
+    fig, ax = plt.subplots(figsize=(20, 20))
+    pos = {node: (points[i][0].item(), points[i][1].item()) for i, node in enumerate(graph.graph.nodes())}
+
+    for node, (x, y) in pos.items():
+        radius = graph.radii[list(graph.graph.nodes()).index(node)].item()
+        polygon = create_polygon(x, y, radius, n_sides)
+        ax.add_patch(polygon)
+        ax.text(x, y, node, ha='center', va='center', fontsize=5)
+
+    for edge in graph.graph.edges():
+        x1, y1 = pos[edge[0]]
+        x2, y2 = pos[edge[1]]
+        ax.plot([x1, x2], [y1, y2], color='gray')
+
+    plt.title(title)
+    plt.axis('equal')
+    plt.axis('off')
+    plt.show()
